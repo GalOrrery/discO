40e6542f62d31ed1d5227b526c435815b6cd04e9
Nathaniel Starkman <nstarman@users.noreply.github.com>
2021-02-26 13:45:30 -0500

    statistics (#50)
    * rms
    * changelog
    
    **only works on the acceleration observeable**
    
    Signed-off-by: Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>

---
+++ discO/core/tests/test_statistic.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""Testing :mod:`~discO.core.statistic`."""
+
+__all__ = [
+    "test_rms",
+]
+
+
+##############################################################################
+# IMPORTS
+
+# THIRD PARTY
+import astropy.coordinates as coord
+import astropy.units as u
+import numpy as np
+
+# PROJECT-SPECIFIC
+from discO.core import statistic
+from discO.utils.vectorfield import CartesianVectorField
+
+##############################################################################
+# TESTS
+##############################################################################
+
+
+def test_rms():
+    """:func:`~discO.core.statistic.rms`"""
+    x = np.linspace(0, 1, num=49) * u.kpc
+    vf_x = np.ones(len(x)) * u.km / u.s
+
+    points = coord.CartesianRepresentation((x, x, x))
+    vf = CartesianVectorField(points, vf_x=vf_x, vf_y=vf_x, vf_z=vf_x)
+
+    got = statistic.rms(vf)
+
+    assert got == np.sqrt(3) * u.km / u.s
+
+
+# /def
+
+
+##############################################################################
+# END
