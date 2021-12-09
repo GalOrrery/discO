daba307954af7cd0066f9eb0501aba1cebb5dc2e
nstarman <nstarkman@protonmail.com>
2021-12-09 17:57:50 +0000

    make table for all fits
    Signed-off-by: nstarman <nstarkman@protonmail.com>

--- discO/data/err_field/script.py
+++ discO/data/err_field/script.py
@@ -252,9 +252,9 @@ def query_and_fit_pixel_set(
     healpix_order: int,
     random_index: T.Optional[int] = 1_000_000,
     *,
+    savetable: QTable,
     plot: bool = True,
     use_local: bool = True,
-    saveloc: pathlib.Path = THIS_DIR,
 ) -> None:
     """Query and fit a set of sky pixels (healpix pixels).
 
@@ -274,39 +274,39 @@ def query_and_fit_pixel_set(
     use_local : bool (optional, keyword-only)
         Whether to perform the query on a local database (`True`, default) or
         on Gaia's servers (`False`).
-    saveloc : `pathlib.Path` (optional, keyword-only)
-        Where to save the fit to the 'pixel'.
+    savetable : `~astropy.table.QTable` (optional, keyword-only)
+        Where to store the fit to the 'pixel'.
     """
-    # create directories
-    PFOLDER = saveloc / f"order_{healpix_order}"
-    PFOLDER.mkdir(exist_ok=True)
+#     # create directories
+#     PFOLDER = saveloc / f"order_{healpix_order}"
+#     PFOLDER.mkdir(exist_ok=True)
+# 
+#     FOLDER = PFOLDER / f"random_{random_index}" if random_index is not None else "allsky"
+#     FOLDER.mkdir(exist_ok=True)
+# 
+#     PLOT_DIR = FOLDER / "figures"
+#     PLOT_DIR.mkdir(exist_ok=True)
+# 
+#     DATA_DIR = FOLDER / "pixel_fits"
+#     DATA_DIR.mkdir(exist_ok=True)
 
-    FOLDER = PFOLDER / f"random_{random_index}" if random_index is not None else "allsky"
-    FOLDER.mkdir(exist_ok=True)
-
-    PLOT_DIR = FOLDER / "figures"
-    PLOT_DIR.mkdir(exist_ok=True)
-
-    DATA_DIR = FOLDER / "pixel_fits"
-    DATA_DIR.mkdir(exist_ok=True)
-
-    empty = np.empty(len(pixel_ids))
-    dtype = [
-        ("pixel_id", "float64"),
-        ("fit_intercept", "bool"),
-        ("normalize", "U10"),
-        ("copy_X", "bool"),
-        ("n_jobs", object),
-        ("positive", "bool"),
-        ("n_features_in_", "i4"),
-        ("coef_", "float64", 3),
-        ("_residues", "float64"),
-        ("rank_", "i4"),
-        ("singular_", "float64", 3),
-        ("intercept_", "float64"),
-        ("_sklearn_version", "U4"),
-    ]
-    fits = QTable(data=np.empty(len(pixel_ids), dtype=dtype))
+    # empty = np.empty(len(pixel_ids))
+    # dtype = [
+    #     ("pixel_id", "int64"),
+    #     ("fit_intercept", "bool"),
+    #     ("normalize", "U10"),
+    #     ("copy_X", "bool"),
+    #     ("n_jobs", object),
+    #     ("positive", "bool"),
+    #     ("n_features_in_", "i4"),
+    #     ("coef_", "float64", 3),
+    #     ("_residues", "float64"),
+    #     ("rank_", "i4"),
+    #     ("singular_", "float64", 3),
+    #     ("intercept_", "float64"),
+    #     ("_sklearn_version", "U4"),
+    # ]
+    # fits = QTable(data=np.empty(len(pixel_ids), dtype=dtype))
 
     shortened = hash(pixel_ids)  # TODO! do better. Put in PDF metadata
 
@@ -336,7 +336,10 @@ def query_and_fit_pixel_set(
     if plot:
         fig = plt.figure()
         plot_mollview(pixel_ids, healpix_order, fig=fig)
-        fig.savefig(PLOT_DIR / f"mollview-{shortened}.pdf")
+        fig.savefig(PLOT_DIR / f"mollview_{shortened}.pdf")
+
+        with open(PLOT_DIR / f"ref_{shortened}.txt", mode="w") as f:
+            f.write(str(pixel_ids))
 
     # -----------------------
     # Fits to each pixel
@@ -354,18 +357,16 @@ def query_and_fit_pixel_set(
     pixel: QTable
     ax: T.Union[plt.axes._subplots.AxesSubplot, None]
     for i, (pixel, ax) in enumerate(zip(pixels.groups, axs.flat)):  # iter thru pixels
-        fit_pixel(pixel, int(pixel[hpl][0]), row=fits[i], ax=ax)
+        fit_pixel(pixel, int(pixel[hpl][0]), row=savetable[i], ax=ax)
 
     # save table
-    fits.write(DATA_DIR / f"fit_{shortened}.ecsv", overwrite=True)
-    # and reference for content of table
-    with open(DATA_DIR / f"ref-{shortened}.txt", mode="w") as f:
-        f.write(str(pixel_ids))
+    # fits.write(DATA_DIR / f"fit_{shortened}.ecsv", overwrite=True)
+    # # and reference for content of table
 
     # save plot of all the pixels
     if plot:
         plt.tight_layout()
-        fig.savefig(PLOT_DIR / f"parallax-{shortened}.pdf")
+        fig.savefig(PLOT_DIR / f"parallax_{shortened}.pdf")
 
 
 def make_groups(
@@ -669,21 +670,61 @@ def main(
         # groups the pixels together so that each group will have
         # approximately the same number of stars.
         list_of_groups = make_groups(sky, healpix_order=ns.order, numgroups=ns.ngroups)
+        npix = nside_to_npix(level_to_nside(ns.order))
     elif ns.pixels_range:
         pi, pf = ns.pixels_range
         if pi >= pf:
             raise ValueError("`pixels_range` must be [start, stop], with stop > start.")
         list_of_groups = np.array_split(np.arange(pi, pf), ns.ngroups)
+        npix = pf - pi
     elif ns.pixels:
         list_of_groups = ns.pixels
+        # npix = # TODO!
 
     # -----------------------
-    # optionally ignore warnings
+    # query and fit
+    # optionlly ignore warnings
+
+    # create directories
+    saveloc = pathlib.Path(ns.saveloc).expanduser().resolve()
+
+    PFOLDER = saveloc / f"order_{ns.order}"
+    PFOLDER.mkdir(exist_ok=True)
+    
+    FOLDER = PFOLDER / f"random_{ns.random_index}" if random_index is not None else "allsky"
+    FOLDER.mkdir(exist_ok=True)
+    
+    PLOT_DIR = FOLDER / "figures"
+    PLOT_DIR.mkdir(exist_ok=True)
+    
+    DATA_DIR = FOLDER / "pixel_fits"
+    DATA_DIR.mkdir(exist_ok=True)
+
+    dtype = [
+        ("pixel_id", "int64"),
+        ("fit_intercept", "bool"),
+        ("normalize", "U10"),
+        ("copy_X", "bool"),
+        ("n_jobs", object),
+        ("positive", "bool"),
+        ("n_features_in_", "i4"),
+        ("coef_", "float64", 3),
+        ("_residues", "float64"),
+        ("rank_", "i4"),
+        ("singular_", "float64", 3),
+        ("intercept_", "float64"),
+        ("_sklearn_version", "U4"),
+    ]
+    fits = QTable(data=np.empty(npix, dtype=dtype))
+    # TODO! save to HDF5 and work with it in append mode so that
+    # each pixel set can be saved as soon as it's done.
+
     with warnings.catch_warnings():
         if ns.filter_warnings:
             warnings.simplefilter("ignore", category=UndefinedMetricWarning)
             warnings.simplefilter("ignore", category=UserWarning)
 
+        running_index = 0
         for batch in tqdm.tqdm(list_of_groups):
             query_and_fit_pixel_set(
                 tuple(batch),
@@ -691,8 +732,12 @@ def main(
                 random_index=ns.random_index,
                 plot=ns.plot,
                 use_local=ns.use_local,
-                saveloc=pathlib.Path(ns.saveloc).expanduser().resolve(),
+                savetable=fits[running_index:running_index+len(batch)],
             )
+            # update starting index
+            running_index += len(batch)
+
+    fits.write(DATA_DIR / f"fits.ecsv", overwrite=True)
 
 
 # ------------------------------------------------------------------------
